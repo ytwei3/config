35087f6267e2d721bd7e630e0014668c8806ca34
weiyaotian <yaotianwei03@gmail.com>
2024-09-17 16:10:05 +0800

    modified:   nvim/.gitignore 	modified:   nvim/init.lua 	new file:   nvim/lua/config/autocmds.lua 	new file:   nvim/lua/config/keymaps.lua 	new file:   nvim/lua/config/lazy.lua 	new file:   nvim/lua/config/options.lua 	deleted:    nvim/lua/custom/plugins/init.lua 	renamed:    nvim/lua/kickstart/plugins/autopairs.lua -> nvim/lua/plugins/autopairs.lua 	new file:   nvim/lua/plugins/copilot.lua 	renamed:    nvim/lua/kickstart/plugins/debug.lua -> nvim/lua/plugins/debug.lua 	renamed:    nvim/lua/kickstart/plugins/gitsigns.lua -> nvim/lua/plugins/gitsigns.lua 	renamed:    nvim/lua/kickstart/plugins/indent_line.lua -> nvim/lua/plugins/indent_line.lua 	renamed:    nvim/lua/kickstart/plugins/lazygit.lua -> nvim/lua/plugins/lazygit.lua 	renamed:    nvim/lua/kickstart/plugins/lint.lua -> nvim/lua/plugins/lint.lua 	renamed:    nvim/lua/kickstart/plugins/neo-tree.lua -> nvim/lua/plugins/neo-tree.lua 	new file:   nvim/lua/plugins/treesitter.lua 	modified:   zsh/aliases.zsh 	modified:   zsh/env.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias lg=lazygit
+alias vi=nvim
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,1 +1,7 @@
 # JAVA
+export JAVA_HOME="/opt/homebrew/opt/openjdk@17"
+export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
+
+# GO
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$PATH"
