fa21f675ba8d7f243d438b0b9e66e79d0fe33041
hikari234 <hikari233@icloud.com>
2024-09-17 23:26:54 +0800

    modified:   zsh/aliases.zsh 	modified:   zsh/env.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,4 @@
-alias lg=lazygit
 alias vi=nvim
+alias lg=lazygit
+alias py=python
+alias ra='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,25 @@
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 # JAVA
 export JAVA_HOME="/opt/homebrew/opt/openjdk@17"
 export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
 
 # GO
-export GOPATH="$HOME/go"
+export GOPATH="$HOME/.go"
 export PATH="$GOPATH/bin:$PATH"
+
+# Rust
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# C++ configuration
+# export PATH="/opt/homebrew/include:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LLVM_ROOT="/opt/homebrew/opt/llvm"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+
+cc="/opt/homebrew/opt/llvm/bin/clang"
+cxx="/opt/homebrew/opt/llvm/bin/clang++"
+
